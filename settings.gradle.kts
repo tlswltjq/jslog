d31dfea8bff62b3fa231c87e76032f9f00346ecb
jiseopshin <wltjq1203@icloud.com>
2025-07-06 23:34:14 +0900

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "jslog_spring"
---
+++ src/main/java/com/jslog_spring/JslogSpringApplication.java
@@ -0,0 +1,13 @@
+package com.jslog_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JslogSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JslogSpringApplication.class, args);
+    }
+
+}
