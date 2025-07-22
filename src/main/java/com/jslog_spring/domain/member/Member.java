796e6f6a8aebc81748a12b9b25a262ed86ab0daa
jiseopshin <wltjq1203@icloud.com>
2025-07-22 15:22:48 +0900

    빌더어노테이션 수정 및 사용자명 업데이트 메서드 추가. 테스트 작성

--- src/main/java/com/jslog_spring/domain/member/Member.java
+++ src/main/java/com/jslog_spring/domain/member/Member.java
@@ -3,16 +3,22 @@ package com.jslog_spring.domain.member;
 import com.jslog_spring.global.jpa.entity.BaseEntity;
 import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.experimental.SuperBuilder;
 
 
 @Entity
-@SuperBuilder
+@Getter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Member extends BaseEntity {
     private String username;
     private String password;
     private String name;
+
+    public void updateName(String name){
+        this.name = name;
+    }
 }
---
+++ src/test/java/com/jslog_spring/domain/member/MemberTest.java
@@ -0,0 +1,44 @@
+package com.jslog_spring.domain.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class MemberTest {
+    @Test
+    @DisplayName("아이디, 비밀번호, 이름을 입력받아 Member 객체를 생성한다.")
+    void memberCreation(){
+        String username = "testUsername";
+        String password = "testPassword";
+        String name = "testName";
+
+        Member member = Member.builder()
+                .username(username)
+                .password(password)
+                .name(name)
+                .build();
+
+        Assertions.assertThat(member).isNotNull();
+        Assertions.assertThat(member.getUsername()).isEqualTo(username);
+        Assertions.assertThat(member.getPassword()).isEqualTo(password);
+        Assertions.assertThat(member.getName()).isEqualTo(name);
+    }
+
+    @Test
+    @DisplayName("새 이름을 입력받아 Member 객체의 이름을 업데이트한다.")
+    void memberUpdate(){
+        String username = "testUsername";
+        String password = "testPassword";
+        String name = "testName";
+        String newName = "updatedName";
+
+        Member member = Member.builder()
+                .username(username)
+                .password(password)
+                .name(name)
+                .build();
+
+        member.updateName(newName);
+        Assertions.assertThat(member.getName()).isEqualTo(newName);
+    }
+}
