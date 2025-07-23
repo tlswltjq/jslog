d173077b7a083544341edf0e73db7fcb934f9fe0
jiseopshin <wltjq1203@icloud.com>
2025-07-23 17:46:05 +0900

    MemberService 구현

--- src/main/java/com/jslog_spring/domain/member/repository/MemberRepository.java
+++ src/main/java/com/jslog_spring/domain/member/repository/MemberRepository.java
@@ -3,5 +3,10 @@ package com.jslog_spring.domain.member.repository;
 import com.jslog_spring.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    boolean existsByUsername(String username);
+
+    Optional<Member> findByUsername(String username);
 }
