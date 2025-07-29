608707a22425137ee0cef604d22a14ee1c67035a
jiseopshin <wltjq1203@icloud.com>
2025-07-29 23:23:33 +0900

    Repository 작성

---
+++ src/main/java/com/jslog_spring/domain/post/repository/PostRepository.java
@@ -0,0 +1,7 @@
+package com.jslog_spring.domain.post.repository;
+
+import com.jslog_spring.domain.post.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Integer> {
+}
