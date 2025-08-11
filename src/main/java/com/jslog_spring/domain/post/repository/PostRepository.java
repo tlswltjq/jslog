ae44649e479a660ea10493bc63923c1abd589ec4
신지섭 <41179427+tlswltjq@users.noreply.github.com>
2025-08-12 01:51:51 +0900

    Revert "feat/post (#7)"
    This reverts commit dd6bc952c6b1f8c2840a956bcaeafe7d182c3905.

--- src/main/java/com/jslog_spring/domain/post/repository/PostRepository.java
+++ src/main/java/com/jslog_spring/domain/post/repository/PostRepository.java
@@ -1,22 +1,9 @@
 package com.jslog_spring.domain.post.repository;
 
 import com.jslog_spring.domain.post.entity.Post;
-import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
-
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import org.springframework.data.domain.Pageable;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
-    Page<Post> findByAuthorId(Long authorId, Pageable pageable);
-
-    Page<Post> findByTitleContainingOrContentContaining(String keyword, String keyword2, Pageable pageable);
-
-    @Query("select p from Post p where p.authorId = :authorId and (p.title like %:keyword% or p.content like %:keyword%)")
-    Page<Post> findByAuthorIdAndKeyword(@Param("authorId") Long authorId, @Param("keyword") String keyword, Pageable pageable);
 }
