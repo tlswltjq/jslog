b5f617f21730d82d5a2ee6addb23a931ba7f874d
jiseopshin <wltjq1203@icloud.com>
2025-08-12 23:48:58 +0900

    test : 픽스쳐를 사용하도록 수정

--- src/test/java/com/jslog_spring/domain/post/PostTest.java
+++ src/test/java/com/jslog_spring/domain/post/PostTest.java
@@ -1,6 +1,9 @@
 package com.jslog_spring.domain.post;
 
+import com.jslog_spring.domain.member.entity.Member;
 import com.jslog_spring.domain.post.entity.Post;
+import fixture.MemberFixture;
+import fixture.PostFixture;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,28 +13,23 @@ class PostTest {
     @Test
     @DisplayName("authorId, title, content를 입력받아 Post 객체를 생성한다.")
     void postCreate() {
-        long authorId = 1L;
-        String title = "test title";
-        String content = "test content";
-
-        Post post = Post.create(authorId, title, content);
+        Member member = MemberFixture.createMemberWithId(1L);
+        Post post = PostFixture.createPost(member);
 
         assertThat(post).isNotNull();
-        assertThat(post.getAuthorId()).isEqualTo(authorId);
-        assertThat(post.getTitle()).isEqualTo(title);
-        assertThat(post.getContent()).isEqualTo(content);
+        assertThat(post.getMember()).isEqualTo(member);
+        assertThat(post.getTitle()).isEqualTo("Test Title");
+        assertThat(post.getContent()).isEqualTo("Test Content");
     }
 
     @Test
     @DisplayName("title, content를 입력받아 Post 객체를 수정한다.")
     void postUpdate() {
-        long authorId = 1L;
-        String title = "test title";
-        String content = "test content";
         String newTitle = "updated title";
         String newContent = "updated content";
 
-        Post post = Post.create(authorId, title, content);
+        Member member = MemberFixture.createMemberWithId(1L);
+        Post post = PostFixture.createPost(member);
 
         post.update(newTitle, newContent);
 
