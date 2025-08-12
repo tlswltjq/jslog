ec7ee3959bfa5848a2345b28a8a20f21462a0491
jiseopshin <wltjq1203@icloud.com>
2025-08-13 00:06:51 +0900

    feat : 픽스쳐메서드 추가

--- src/test/java/fixture/PostFixture.java
+++ src/test/java/fixture/PostFixture.java
@@ -10,8 +10,19 @@ public class PostFixture {
         return Post.create(member, "Test Title", "Test Content");
     }
 
+
+    public static Post createPost(Member member, String title, String content) {
+        return Post.create(member, title, content);
+    }
+
     public static Post createPostWithId(Long id, Member member) {
         Post post = createPost(member);
+        ReflectionTestUtils.setField(post, "id", id);
+        return post;
+    }
+
+    public static Post createPostWithId(Long id, Member member, String title, String content) {
+        Post post = createPost(member, title, content);
         ReflectionTestUtils.setField(post, "id", id);
         return post;
     }
