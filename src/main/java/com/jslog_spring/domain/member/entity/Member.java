11daf805cebf79b002c154d0805e006d44d567c1
jiseopshin <wltjq1203@icloud.com>
2025-08-12 23:38:44 +0900

    fix : Member와 Post의 연관관계 설정

--- src/main/java/com/jslog_spring/domain/member/entity/Member.java
+++ src/main/java/com/jslog_spring/domain/member/entity/Member.java
@@ -1,8 +1,12 @@
 package com.jslog_spring.domain.member.entity;
 
+import com.jslog_spring.domain.post.entity.Post;
 import com.jslog_spring.global.jpa.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Entity
@@ -16,11 +20,15 @@ public class Member extends BaseEntity {
     @Column(nullable = false)
     private String name;
 
+    @OneToMany(mappedBy = "member", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Post> posts;
+
     @Builder(access = AccessLevel.PRIVATE)
     private Member(String username, String password, String name) {
         this.username = username;
         this.password = password;
         this.name = name;
+        this.posts = new ArrayList<>();
     }
 
     public static Member create(String username, String password, String name) {
@@ -37,5 +45,18 @@ public class Member extends BaseEntity {
 
     public void updateName(String name) {
         this.name = name;
+    }
+
+    public Post createPost(String title, String content) {
+        Post newPost = Post.create(this, title, content);
+         this.posts.add(newPost);
+         return newPost;
+    }
+
+    public void deletePost(Post post) {
+        if (post == null || !this.posts.contains(post)) {
+            throw new IllegalArgumentException("Post not found in member's posts.");
+        }
+        this.posts.remove(post);
     }
 }
