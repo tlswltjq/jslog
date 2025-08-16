dd2b931d922c5e17731054b9d0344219ede55fd9
jiseopshin <wltjq1203@icloud.com>
2025-08-16 17:30:52 +0900

    refactor : join()이 변경된 인터페이스를 따르고 비밀번호를 인코딩 하도록 리팩토링

--- src/main/java/com/jslog_spring/domain/member/service/MemberServiceImpl.java
+++ src/main/java/com/jslog_spring/domain/member/service/MemberServiceImpl.java
@@ -7,17 +7,21 @@ import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 @Service
 @RequiredArgsConstructor
 public class MemberServiceImpl implements MemberService {
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public void join(String username, String password, String name) {
-        Member member = Member.create(username, password, name);
+    public Member join(String username, String password, String name) {
+        String encodedPassword = passwordEncoder.encode(password);
+        Member member = Member.create(username, encodedPassword, name);
         if (memberRepository.existsByUsername(username)) {
             throw new IllegalArgumentException("Username already exists.");
         }
-        memberRepository.save(member);
+        return memberRepository.save(member);
     }
 
     public Member getMember(String username) {
