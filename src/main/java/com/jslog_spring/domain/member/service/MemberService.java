1b0d501e3780716388dd40530ca613fdaf6fbf37
jiseopshin <wltjq1203@icloud.com>
2025-08-16 17:29:06 +0900

    refactor : join() 반환타입 변경 void -> Member

--- src/main/java/com/jslog_spring/domain/member/service/MemberService.java
+++ src/main/java/com/jslog_spring/domain/member/service/MemberService.java
@@ -3,7 +3,7 @@ package com.jslog_spring.domain.member.service;
 import com.jslog_spring.domain.member.entity.Member;
 
 public interface MemberService {
-    void join(String username, String password, String name);
+    Member join(String username, String password, String name);
     Member getMember(String username);
     Member updateUserName(Member member, String newName);
 }
