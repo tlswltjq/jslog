e3da661eee71f4e2e8a0df4d1cddb38c0c1cee21
jiseopshin <wltjq1203@icloud.com>
2025-08-26 15:33:49 +0900

    refactor : 에러 로그로 처리

--- src/main/java/com/jslog_spring/global/security/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/jslog_spring/global/security/JwtAuthenticationEntryPoint.java
@@ -4,14 +4,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jslog_spring.global.response.ApiResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
-@Slf4j
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
     private final ObjectMapper objectMapper = new ObjectMapper();
@@ -21,8 +19,7 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
                          HttpServletResponse response,
                          AuthenticationException authException) throws IOException {
 
-        log.info(authException.getMessage());
-        ApiResponse<?> body = ApiResponse.failure("401", "인증 오류");
+        ApiResponse<?> body = ApiResponse.failure("401", "인증 오류 : " + authException.getMessage());
 
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         response.setContentType("application/json;charset=UTF-8");
