a3d985315540af303f7292c4b9c612de84d6c6e0
jiseopshin <wltjq1203@icloud.com>
2025-08-27 00:39:08 +0900

    feat: 회원 정보 조회 응답 DTO 수정

--- src/main/java/com/jslog_spring/domain/member/dto/MemberInfoResponse.java
+++ src/main/java/com/jslog_spring/domain/member/dto/MemberInfoResponse.java
@@ -1,4 +1,13 @@
 package com.jslog_spring.domain.member.dto;
 
-public record MemberInfoResponse() {
+import com.jslog_spring.domain.member.entity.Member;
+
+public record MemberInfoResponse(
+        String name
+) {
+    public MemberInfoResponse (Member member){
+        this(
+                member.getName()
+        );
+    }
 }
