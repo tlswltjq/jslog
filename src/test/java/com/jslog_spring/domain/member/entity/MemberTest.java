f3244c4873363a55d879c8cf86c25cb0427712d6
jiseopshin <wltjq1203@icloud.com>
2025-09-08 10:23:14 +0900

    chore : 위치 변경

--- src/test/java/com/jslog_spring/domain/member/entity/MemberTest.java
+++ src/test/java/com/jslog_spring/domain/member/entity/MemberTest.java
@@ -1,6 +1,5 @@
-package com.jslog_spring.domain.member;
+package com.jslog_spring.domain.member.entity;
 
-import com.jslog_spring.domain.member.entity.Member;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
