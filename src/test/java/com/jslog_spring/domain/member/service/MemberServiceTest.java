52f31c1c9a09acbf17961e57c47c8e85602067a9
jiseopshin <wltjq1203@icloud.com>
2025-09-08 10:25:20 +0900

    test : 테스트용 MOCK객체 추가

--- src/test/java/com/jslog_spring/domain/member/service/MemberServiceTest.java
+++ src/test/java/com/jslog_spring/domain/member/service/MemberServiceTest.java
@@ -1,4 +1,28 @@
 package com.jslog_spring.domain.member.service;
 
+import com.jslog_spring.domain.member.repository.MemberRepository;
+import com.jslog_spring.global.security.JwtUtil;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@ExtendWith(MockitoExtension.class)
 public class MemberServiceTest {
+    @Mock
+    private MemberRepository memberRepository;
+    @Mock
+    private PasswordEncoder passwordEncoder;
+    @Mock
+    private AuthenticationManager authenticationManager;
+    @Mock
+    private JwtUtil jwtUtil;
+    @Mock
+    private StringRedisTemplate redisTemplate;
+    @InjectMocks
+    private MemberService memberService;
+
 }
