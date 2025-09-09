f8d25c7159824091fdb446d71036c54fcd8f97a4
jiseopshin <wltjq1203@icloud.com>
2025-09-09 12:23:04 +0900

    test : 테스트명 변경 및 테스트 추가

--- src/test/java/com/jslog_spring/domain/member/entity/MemberTest.java
+++ src/test/java/com/jslog_spring/domain/member/entity/MemberTest.java
@@ -6,8 +6,8 @@ import org.junit.jupiter.api.Test;
 
 class MemberTest {
     @Test
-    @DisplayName("아이디, 비밀번호, 이름을 입력받아 Member 객체를 생성한다.")
-    void memberCreation() {
+    @DisplayName("username(아이디), password, name을 입력받아 Member 객체를 생성한다.")
+    void memberCreationTest() {
         String username = "testUsername";
         String password = "testPassword";
         String name = "testName";
@@ -18,11 +18,57 @@ class MemberTest {
         Assertions.assertThat(member.getUsername()).isEqualTo(username);
         Assertions.assertThat(member.getPassword()).isEqualTo(password);
         Assertions.assertThat(member.getName()).isEqualTo(name);
+        Assertions.assertThat(member.getRole()).isEqualTo("ROLE_USER");
+    }
+
+    @Test
+    @DisplayName("Member객체 생성 시 username이 null이거나 빈 문자열일 경우 예외가 발생한다.")
+    void memberCreationInvalidUsernameTest() {
+        String password = "testPassword";
+        String name = "testName";
+
+        Assertions.assertThatThrownBy(() -> Member.create(null, password, name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Username cannot be empty.");
+
+        Assertions.assertThatThrownBy(() -> Member.create("", password, name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Username cannot be empty.");
+    }
+
+    @Test
+    @DisplayName("Member객체 생성 시 password가 null이거나 빈 문자열일 경우 예외가 발생한다.")
+    void memberCreationInvalidPasswordTest() {
+        String username = "testUsername";
+        String name = "testName";
+
+        Assertions.assertThatThrownBy(() -> Member.create(username, null, name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Password cannot be empty.");
+
+        Assertions.assertThatThrownBy(() -> Member.create(username, "", name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Password cannot be empty.");
+    }
+
+    @Test
+    @DisplayName("Member객체 생성 시 name이 null이거나 빈 문자열일 경우 예외가 발생한다.")
+    void memberCreationInvalidNameTest() {
+        String username = "testUsername";
+        String password = "testPassword";
+
+        Assertions.assertThatThrownBy(() -> Member.create(username, password, null))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Name cannot be empty.");
+
+        Assertions.assertThatThrownBy(() -> Member.create(username, password, ""))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Name cannot be empty.");
     }
 
     @Test
     @DisplayName("새 이름을 입력받아 Member 객체의 이름을 업데이트한다.")
-    void memberUpdate() {
+    void memberUpdateTest() {
         String username = "testUsername";
         String password = "testPassword";
         String name = "testName";
