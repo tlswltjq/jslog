c0b1bc77ff8b009193cd0044cfb64bc505f8d151
jiseopshin <wltjq1203@icloud.com>
2026-01-25 19:28:38 +0900

    어드바이스 패키지 수정

--- src/main/java/com/jslog_spring/common/rest/GlobalResponseAdvice.java
+++ src/main/java/com/jslog_spring/common/rest/GlobalResponseAdvice.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
 @Slf4j
-@RestControllerAdvice(basePackages = "com.fivelogic_recreate")
+@RestControllerAdvice(basePackages = "com.jslog_spring")
 public class GlobalResponseAdvice implements ResponseBodyAdvice<ApiResponse<?>> {
     @Override
     public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
