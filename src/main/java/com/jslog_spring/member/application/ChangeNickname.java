ffc759ec4a4207043b1b08efbdd3da54d1a7fbaf
jiseopshin <wltjq1203@icloud.com>
2026-01-26 15:41:11 +0900

    닉네임 변경기능 작성

---
+++ src/main/java/com/jslog_spring/member/application/ChangeNickname.java
@@ -0,0 +1,25 @@
+package com.jslog_spring.member.application;
+
+import com.jslog_spring.member.domain.model.Member;
+import com.jslog_spring.member.domain.policy.MemberPolicy;
+import com.jslog_spring.member.domain.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class ChangeNickname {
+    private final List<MemberPolicy> memberPolicies;
+    private final MemberRepository repository;
+
+    public void invoke(Long memberId, String nickname) {
+        Member member = repository.findById(memberId);
+        member.changeNickname(nickname);
+
+        memberPolicies.forEach(policy -> policy.validate(member));
+    }
+}
