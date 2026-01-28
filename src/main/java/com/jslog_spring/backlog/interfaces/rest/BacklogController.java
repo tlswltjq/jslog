339c093510a07105d9c9b38e526599c8e766a5c2
jiseopshin <wltjq1203@icloud.com>
2026-01-29 00:14:20 +0900

    Authentication적용

--- src/main/java/com/jslog_spring/backlog/interfaces/rest/BacklogController.java
+++ src/main/java/com/jslog_spring/backlog/interfaces/rest/BacklogController.java
@@ -1,17 +1,16 @@
 package com.jslog_spring.backlog.interfaces.rest;
 
-import java.util.List;
-
+import com.jslog_spring.auth.domain.model.AuthUserDetails;
+import com.jslog_spring.backlog.application.*;
 import com.jslog_spring.backlog.application.dto.BacklogInfo;
 import com.jslog_spring.backlog.interfaces.rest.dto.*;
-import com.jslog_spring.backlog.application.*;
-import com.jslog_spring.backlog.interfaces.rest.dto.BacklogCreateRequest;
-import com.jslog_spring.backlog.interfaces.rest.dto.BacklogResponse;
-import com.jslog_spring.backlog.interfaces.rest.dto.BacklogUpdateRequest;
 import com.jslog_spring.common.rest.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -26,9 +25,10 @@ public class BacklogController {
     private final GetAllMyBacklogs getAllMyBacklogs;
 
     @PostMapping
-    public ApiResponse<CreateBacklogResponse> create(@RequestBody BacklogCreateRequest request) {
+    public ApiResponse<CreateBacklogResponse> create(@AuthenticationPrincipal AuthUserDetails authUser,
+                                                     @RequestBody BacklogCreateRequest request) {
         Long backlogId = addBacklog.invoke(
-                1L,
+                authUser.getAccountId(),
                 request.name(),
                 request.desc(),
                 request.dueDate());
@@ -36,10 +36,9 @@ public class BacklogController {
     }
 
     @PutMapping("/{backlogId}")
-    public ApiResponse<UpdateBacklogResponse> update(@PathVariable Long backlogId,
-            @RequestBody BacklogUpdateRequest request) {
+    public ApiResponse<UpdateBacklogResponse> update(@AuthenticationPrincipal AuthUserDetails authUser, @PathVariable Long backlogId, @RequestBody BacklogUpdateRequest request) {
         Long id = editBacklog.invoke(
-                1L,
+                authUser.getAccountId(),
                 backlogId,
                 request.name(),
                 request.desc(),
@@ -48,32 +47,32 @@ public class BacklogController {
     }
 
     @DeleteMapping("/{backlogId}")
-    public ApiResponse<DeleteBacklogResponse> delete(@PathVariable Long backlogId) {
-        deleteBacklog.invoke(1L, backlogId);
+    public ApiResponse<DeleteBacklogResponse> delete(@AuthenticationPrincipal AuthUserDetails authUser, @PathVariable Long backlogId) {
+        deleteBacklog.invoke(authUser.getAccountId(), backlogId);
         return ApiResponse.success(HttpStatus.OK.value(), "백로그 삭제 성공", DeleteBacklogResponse.from(backlogId));
     }
 
     @PatchMapping("/{backlogId}/done")
-    public ApiResponse<DoneBacklogResponse> done(@PathVariable Long backlogId) {
-        Long id = doneBacklog.invoke(1L, backlogId);
+    public ApiResponse<DoneBacklogResponse> done(@AuthenticationPrincipal AuthUserDetails authUser, @PathVariable Long backlogId) {
+        Long id = doneBacklog.invoke(authUser.getAccountId(), backlogId);
         return ApiResponse.success(HttpStatus.OK.value(), "백로그 완료 처리 성공", DoneBacklogResponse.from(id));
     }
 
     @PatchMapping("/{backlogId}/undone")
-    public ApiResponse<UndoneBacklogResponse> undone(@PathVariable Long backlogId) {
-        Long id = unDoneBacklog.invoke(1L, backlogId);
+    public ApiResponse<UndoneBacklogResponse> undone(@AuthenticationPrincipal AuthUserDetails authUser, @PathVariable Long backlogId) {
+        Long id = unDoneBacklog.invoke(authUser.getAccountId(), backlogId);
         return ApiResponse.success(HttpStatus.OK.value(), "백로그 미완료 처리 성공", UndoneBacklogResponse.from(id));
     }
 
     @GetMapping("/{backlogId}")
-    public ApiResponse<BacklogResponse> getInfo(@PathVariable Long backlogId) {
-        BacklogInfo backlog = getInfoOfBacklog.invoke(1L, backlogId);
+    public ApiResponse<BacklogResponse> getInfo(@AuthenticationPrincipal AuthUserDetails authUser, @PathVariable Long backlogId) {
+        BacklogInfo backlog = getInfoOfBacklog.invoke(authUser.getAccountId(), backlogId);
         return ApiResponse.success(HttpStatus.OK.value(), "백로그 조회 성공", BacklogResponse.from(backlog));
     }
 
     @GetMapping
-    public ApiResponse<List<BacklogResponse>> getAll() {
-        List<BacklogInfo> backlogs = getAllMyBacklogs.invoke(1L);
+    public ApiResponse<List<BacklogResponse>> getAll(@AuthenticationPrincipal AuthUserDetails authUser) {
+        List<BacklogInfo> backlogs = getAllMyBacklogs.invoke(authUser.getAccountId());
         return ApiResponse.success(HttpStatus.OK.value(), "내 백로그 전체 조회 성공", backlogs.stream()
                 .map(BacklogResponse::from)
                 .toList());
